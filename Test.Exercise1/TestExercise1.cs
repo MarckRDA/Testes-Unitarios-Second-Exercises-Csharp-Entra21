91934af80060aafbb842ae7719e23596a8e20a51
MarckRDA <marcos.edo132@gmail.com>
2020-09-17 20:32:26 -0300

    Code refactored

--- Test.Exercise1/TestExercise1.cs
+++ Test.Exercise1/TestExercise1.cs
@@ -5,23 +5,14 @@ namespace Test.Exercise1
 {
     public class TestExercise1
     {
-        [Fact]
-        public void Should_Return_A_Subtraction_Between_ArrayA_And_ArrayB()
+        [Theory]
+        [InlineData(new double[] {1,5,4,2,6,7,8,9,4,3,2,1,2,3,4}, new double[]{5,7,8,9,3,2,1,3,4,5,7,9,0,1,2})]
+        public void Should_Return_A_Subtraction_Between_ArrayA_And_ArrayB(double[] arrayA, double[] arrayB)
         {
             //Given
             var exercise1 = new Exercise1();
 
             //When
-            var arrayA = new double[15]
-            {
-                1,5,4,2,6,7,8,9,4,3,2,1,2,3,4
-            };
-
-            var arrayB = new double[15]
-            {
-                5,7,8,9,3,2,1,3,4,5,7,9,0,1,2
-            };
-
             var result = exercise1.ArrayCMixUp(arrayA, arrayB);
 
             //Then
